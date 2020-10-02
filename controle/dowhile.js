c55e70cad7993eac19bbea2ee2e7468eef20bb5d
Nadirlene <nadirleneoliveira@yahoo.com>
2020-10-02 16:10:05 -0300

    summ

--- controle/dowhile.js
+++ controle/dowhile.js
@@ -1,7 +1,13 @@
-function getInteiroAleatorioEntre(min, max) {
+function getInteiroAleatorioentre(min, max) {
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
 
-let opcao = 0
+let opcao = -1
 
+ do {
+    opcao = getInteiroAleatorioentre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+} while (opcao != -1)
+
+console.log('Até logo!!!!')
