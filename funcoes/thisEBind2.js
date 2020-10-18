a72e361bd2b6db91919c2ea165cb448329efcac9
Nadirlene <70604780+Nadirlene@users.noreply.github.com>
2020-10-18 08:43:04 -0300

    exemplo funções

---
+++ funcoes/thisEBind2.js
@@ -0,0 +1,12 @@
+//dispara outra função a partir de um intervalo que foi passado
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this) */, 1000)
+}
+
+new Pessoa
