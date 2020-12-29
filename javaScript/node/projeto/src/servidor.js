fb420df9c6a955f47bfb0f2c86699e556cc0ee9a
Nadirlene <70604780+Nadirlene@users.noreply.github.com>
2020-12-29 00:07:52 -0300

    hh

---
+++ javaScript/node/projeto/src/servidor.js
@@ -0,0 +1,17 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bancoDeDados = require('./bancoDeDados')
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está na porta ${porta}.`)
+})
