18d3f0b33e613d6e739fbef1c76a10a6e33ec8a9
Nadirlene <70604780+Nadirlene@users.noreply.github.com>
2020-12-30 09:36:48 -0300

    ...

---
+++ javaScript/esnext/revisao1.js
@@ -0,0 +1,10 @@
+// let e const
+// let escopo de função e bloco
+// variável declarada com var não tem excopo de bloco então consigo acessar ela de fora do bloco
+
+{
+    var a = 2
+    let b = 3
+}
+
+console.log(a)
---
+++ javaScript/node/temporizador.js
@@ -0,0 +1,23 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * 17 * * 2', function() {
+    console.log('Executando Tarefa 1!', new Date().getSeconds())
+})
+// segundos, hora, dia do mês, mês, dia da semana
+
+setTimeout(function () {
+    tarefa1.cancel()
+console.log('Cancelando Tarefa 1!')
+}, 20000)
+
+// setImmediate
+// setInterval
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 17
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function() {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
